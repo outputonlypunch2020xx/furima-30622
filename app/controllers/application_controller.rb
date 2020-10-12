31bd613dbb2010891a461b0fddc519e8b6537b29
outputonlypunch2020xx <outputonlypunch2020xx@gmail.com>
2020-10-12 15:29:11 +0900

    ユーザ管理機能実装完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   # before_action :basic_auth
+  #before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:birthday,:family_name,:first_name,:family_name_kana,:first_name_kana])
+  end
+
+
+
+
+  
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'items#index'
   resources :items do
   end
+  resources :users, only: [:edit, :update]
 end
