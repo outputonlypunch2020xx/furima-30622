46082a572dce14736eef96da7fefabc9626d7208
outputonlypunch2020xx <outputonlypunch2020xx@gmail.com>
2020-10-16 14:27:09 +0900

    修正what,why

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @items = Item.all
     @items = Item.includes(:user).order('created_at DESC')
   end
 
@@ -21,9 +20,6 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
-    @item = Item.find(params[:id])
-  end
 
   private
 
