d15085a27096dd5628be8dbb4351d9b7ef22fac0
outputonlypunch2020xx <outputonlypunch2020xx@gmail.com>
2020-10-16 11:08:38 +0900

    修正less_than_or_equal_to

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,11 +17,11 @@ class Item < ApplicationRecord
       validates :shipping_days_id 
       validates :shipping_fee_id
       end
-      
+
     validates :area_id, numericality: { other_than: 0 }
     validates :price, numericality: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters.' }
     validates :price, numericality: { greater_than: 299 }
-    validates :price, numericality: { less_than: 9_999_999 }
+    validates :price, numericality: { less_than_or_equal_to: 9_999_999 }
   end
 
   has_one_attached :image
