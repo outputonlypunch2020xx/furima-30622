b94f4be8162402dc349401c493a1e962210ca5b0
outputonlypunch2020xx <outputonlypunch2020xx@gmail.com>
2020-10-16 10:59:16 +0900

    修正withoption

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,18 +7,18 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shippingfee
 
   with_options presence: true do
-   validates :area_id, numericality: { other_than: 0 }
-    validates :category_id, numericality: { other_than: 1 }
-    validates :item_status_id, numericality: { other_than: 1 }
-    validates :shipping_days_id, numericality: { other_than: 1 }
-    validates :shipping_fee_id, numericality: { other_than: 1 }
     validates :name 
     validates :text 
-    validates :image
-  end
-
-
-  with_options presence: true do
+    validates :image    
+    
+      with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :item_status_id
+      validates :shipping_days_id 
+      validates :shipping_fee_id
+      end
+      
+    validates :area_id, numericality: { other_than: 0 }
     validates :price, numericality: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters.' }
     validates :price, numericality: { greater_than: 299 }
     validates :price, numericality: { less_than: 9_999_999 }
