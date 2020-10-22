809acfcacfd1e74048e51264c4a33c3e36510e4f
outputonlypunch2020xx <outputonlypunch2020xx@gmail.com>
2020-10-22 12:01:19 +0900

    追加テストコードの実行

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-//require("turbolinks").start()
+//require("turbolinks").start()        後の実装に役立てるため
 require("@rails/activestorage").start()
 require("channels")
 require("../keisan.js");
