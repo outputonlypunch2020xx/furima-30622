b2d87d0ed6e6c49834ef5ada9efd0888fee55bc3
outputonlypunch2020xx <outputonlypunch2020xx@gmail.com>
2020-11-02 14:49:22 +0900

    修正３

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,11 +16,11 @@ class UsersController < ApplicationController
     @items = user.items
     @user = User.find(params[:id])
   
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-    card = Card.find_by(user_id: current_user.id)
-    redirect_to new_card_path and return unless card.present?
-    customer = Payjp::Customer.retrieve(card.customer_token)
-    @card = customer.cards.first
+    #Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    #card = Card.find_by(user_id: current_user.id)
+    #redirect_to new_card_path and return unless card.present?
+    #customer = Payjp::Customer.retrieve(card.customer_token)
+    #@card = customer.cards.first
   end
 
   private
